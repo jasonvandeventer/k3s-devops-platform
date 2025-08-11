f251adbb3314ef724f9d3193ef4761bdbe7a8476
Jason <jason@vanfreckle.com>
2025-08-11 16:57:09 -0500

    feat(app): v2 text

--- app/main.go
+++ app/main.go
@@ -13,7 +13,7 @@ func main() {
 		host, _ := os.Hostname()
 		fmt.Fprintf(
 			w,
-			"<h1>k3s demo app</h1><p>version: %s</p><p>host: %s</p><p>time: %s</p>",
+			"<h1>k3s demo app v2</h1><p>version: %s</p><p>host: %s</p><p>time: %s</p>",
 			version, host, time.Now().Format(time.RFC3339),
 		)
 	})
